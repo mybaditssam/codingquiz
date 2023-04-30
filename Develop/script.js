87baa3090456c18d6348dc8845ded0c0b4d22617
mybaditssam <shernandez@econnect.eco>
2023-04-30 15:05:22 -0400

    Questions are displaying, but bug does not display correct answer, all coming up as incorrect

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,99 @@
-var questiondisplay = document.querySelector("#quizpage")
-var welcomepage = document.querySelector("#welcomepage")
-var startbtn = document.querySelector("#startbtn")
-var quizTimer = document.querySelector("#timer")
+// All global variables
+var questiondisplay = document.querySelector("#quizpage");
+var welcomepage = document.querySelector("#welcomepage");
+var startbtn = document.querySelector("#startbtn");
+var quizTimer = document.querySelector("#timer");
 var timeLeft = 60;
+var codingquiztitle = document.querySelector('h1');
+var btnA = document.getElementById('a');
+var btnB = document.getElementById('b');
+var btnC = document.getElementById('c');
+var btnD = document.getElementById('d');
+var quizmain = document.getElementById('quiz');
+var score = 0;
+var result = document.getElementById('result')
+var correct;
 
-console.log(startbtn)
+//console log to check if selectors are working
+console.log(startbtn);
 
 
 var questions = [
-  { question: "What is JavaScript?", 
-    answers: ["A markup language", "A programming language", "A stylesheet language", "A database language"], 
-    correctAnswer: "A programming language" },
-
-  { question: "What is the correct syntax for a JavaScript comment?", 
-    answers: ["<!--This is a comment-->", "//This is a comment", "/*This is a comment*/", "#This is a comment"], 
-    correctAnswer: "//This is a comment" },
-
-  { question: "Which statement is used to declare a variable?", 
-  answers: ["let", "var", "const", "All of the above"], 
-  correctAnswer: "All of the above" },
-
-  { question: "Which data type is used to represent decimal numbers?", 
-  answers: ["String", "Number", "Boolean", "Undefined"], 
-  correctAnswer: "Number" },
-
-  { question: "What does the '===' operator do in JavaScript?", 
-    answers: ["Checks if two values are equal", "Checks if two values are equal and of the same type", "Checks if two values are not equal", "Checks if two values are not equal and of the same type"], 
-    correctAnswer: "Checks if two values are equal and of the same type" },
-
-  { question: "Which statement is used to create a loop?", 
-    answers: ["if", "else", "while", "for"], 
-    correctAnswer: "for" }
+  {
+    question: 'What is JavaScript?',
+    answer1: 'A markup language',
+    answer2: 'A programming language',
+    answer3: 'A stylesheet language',
+    answer4: 'A database language',
+    correctAnswer: 'A programming language'
+  },
+  {
+    question: 'What is the correct syntax for a JavaScript comment?',
+    answer1: '<!--This is a comment-->',
+    answer2: '//This is a comment',
+    answer3: '/*This is a comment*/',
+    answer4: '#This is a comment',
+    correctAnswer: '//This is a comment'
+  },
+  {
+    question: 'Which statement is used to declare a variable?',
+    answer1: 'let',
+    answer2: 'var',
+    answer3: 'const',
+    answer4: 'All of the above',
+    correctAnswer: 'All of the above'
+  },
+  {
+    question: 'Which data type is used to represent decimal numbers?',
+    answer1: 'String',
+    answer2: 'Number',
+    answer3: 'Boolean',
+    answer4: 'Undefined',
+    correctAnswer: 'Number'
+  },
+  {
+    question: 'What does the === operator do in JavaScript?',
+    answer1: 'Checks if two values are equal',
+    answer2: 'Checks if two values are equal and of the same type',
+    answer3: 'Checks if two values are not equal',
+    answer4: 'Checks if two values are not equal and of the same type',
+    correctAnswer: 'Checks if two values are equal and of the same type'
+  },
+  {
+    question: 'Which statement is used to create a loop?',
+    answer1: 'if',
+    answer2: 'else',
+    answer3: 'while',
+    answer4: 'for',
+    correctAnswer: 'for'
+  }
 ];
 
+var lastquestion = questions.length;
+var questionON = 0;
+var questiontext = document.getElementById('questions');
 
+function displayquestion () {
+  var currentQuestion = questions[questionON];
+  console.log(currentQuestion)
+  questiontext.innerHTML = '<p>' + currentQuestion.question + '</p>';
+  btnA.innerHTML = currentQuestion.answer1;
+  btnB.innerHTML = currentQuestion.answer2;
+  btnC.innerHTML = currentQuestion.answer3;
+  btnD.innerHTML = currentQuestion.answer4;
+  correct = currentQuestion.correctAnswer;
+}
+
+// hides h1, Start Quiz btn && begins quiz/timer
 function letsgo () {
-  startbtn.style.display = "none"
-  // need to figure this SHIT OUT BRUH display questions
+  startbtn.style.display = 'none'
+  codingquiztitle.style.display ='none'
+  displayquestion();
+  quizmain.style.display = 'block';
 
-  //timer starts
   timerInterval = setInterval(function() {
     timeLeft--;
-    quizTimer.textContent = "Time left: " + timeLeft;
+    quizTimer.textContent = 'Time left: ' + timeLeft;
 
     if(timeLeft === 0) {
       clearInterval(timerInterval);
@@ -50,10 +102,30 @@ function letsgo () {
   }, 1000);
 }
 
+function checkAnswer(answer) {
+  var currentQuestion = questions[questionON];
+  console.log (currentQuestion)
+  var correct = currentQuestion.correctAnswer;
+  console.log(correct)
 
-startbtn.addEventListener("click", letsgo)
+  if (answer === correct && questionON !== lastquestion - 1){
+    score++;
+    result.textContent = ('you are Correct!')
+    questionON++;
+    displayquestion();
+  } 
+    else if (answer !== correct && questionON !== lastquestion - 1){
+      result.textContent ='You are Incorrect'
+    questionON++;
+    displayquestion();
+  
+  } 
+    else {
+    result.textContent = 'You are Incorrect'
+    clearInterval(timerInterval);
+    
+  }
+}
 
 
-// 1. Add query selector to start button
-// 2. add event listener to remove start button when clicked
-// 3. Create function to remove #welcomepage
+startbtn.addEventListener('click', letsgo)
